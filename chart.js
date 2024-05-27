6fe358172041d4d6891167bd6be742572e1bf9cf
Anas Abdulhaq <103602962@student.swin.edu.au>
2024-05-27 22:55:27 +1000

    Base Files Template

--- chart.js
+++ chart.js
@@ -1,4 +1,17 @@
 function init(){
 
+    var w = 1000;
+    var h = 600;
+
+    var projection = d3.geoMercator();
+
+    var path = d3.geoPath()
+                .projection(projection);
+
+    var svg = d3.select("#chart")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+    
 }
 window.onload = init;

a06716058104e78ad43dde758ef9338668eab951
Anas Abdulhaq <61099687+Cleo5779@users.noreply.github.com>
2024-06-03 21:18:31 +1000

    Base world map displayed

--- chart.js
+++ chart.js
@@ -3,7 +3,10 @@ function init(){
     var w = 1000;
     var h = 600;
 
-    var projection = d3.geoMercator();
+    var projection = d3.geoEquirectangular()
+                        .scale([130])
+                        .center([0, 20])
+                        .translate([w / 2, h / 2]);
 
     var path = d3.geoPath()
                 .projection(projection);
@@ -11,7 +14,17 @@ function init(){
     var svg = d3.select("#chart")
                 .append("svg")
                 .attr("width", w)
-                .attr("height", h);
+                .attr("height", h)
+                .attr("fill", "grey");
+
+    d3.json("globe.json").then(function(json) {
+
+        svg.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path);
+    })
     
 }
 window.onload = init;
